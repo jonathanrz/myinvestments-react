6750922cb8cbddecda6a41a4e39e22cbefa15635
Jonathan Rafael Zanella <jonathanrafaelz@gmail.com>
2017-09-19 21:15:00 -0300

    Show mocked investments

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const investments = [
+  {
+      id: "58f3f5d4524b280012157c09",
+      holder: "Bradesco",
+      type: "Tesouro Direto",
+      name: "Tesouro IPCA+ 2019 (NTNB Princ)",
+      due_date: "2019-05-15T00:00:00.000Z"
+  },
+  {
+    id: "58f41036b740dc0012ecbf2b",
+    holder: "Easynvest",
+    type: "CDB",
+    name: "Banco Máxima",
+    due_date: "2019-04-05T00:00:00.000Z"
+  },
+  {
+    id: "598488aeac0e3010116f3fbb",
+    holder: "Clear",
+    type: "Ação",
+    name: "TUPY3"
+  }
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { investments.map(item =>
+          <div key={item.id}>
+            <span>{item.name}</span>
+            <span>{item.type}</span>
+            <span>{item.holder}</span>
+            <span>{item.due_date}</span>
+          </div>
+        )}
       </div>
     );
   }
